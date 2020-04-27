5e53546d75ea1a2d32f29e9e7dff740a4890ae0a
Naeem Khan <naeemukhan14@gmail.com>
2020-04-27 09:30:27 +0400

    Channel creation on command added.

--- host_channel_creator/main.py
+++ host_channel_creator/main.py
@@ -11,13 +11,23 @@ class CustomClient(discord.Client):
     # Print in console that the bot has connected to server
     async def on_ready(self):
         print(f'{self.user} has connected to Discord!')
-
+        
     # DM the new user a welcome message on join
     async def on_member_join(self, member):
         await member.create_dm()
         await member.dm_channel.send(
             f'Hi {member.name}, welcome to this amazingly empty discord server'
         )
+
+    async def on_message(self, message):
+        server = discord.utils.get(self.guilds, name='Bot Testing')
+        # Ignore self messages
+        if message.author == client.user:
+            return
+        # Respond to only those messages that start with given prefix
+        if message.content.startswith('$t'):
+            await message.channel.send("Channel named " + message.content.split(' ')[-1] + " has been created") # TODO: add error-handling
+            await server.create_text_channel(message.content.split(' ')[-1])
 
 
 # Initialize class object

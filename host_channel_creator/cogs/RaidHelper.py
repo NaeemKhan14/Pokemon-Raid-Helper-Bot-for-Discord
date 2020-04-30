014141776f22eafa60f7659b00c910f66a7e1c1d
Naeem Khan <naeemukhan14@gmail.com>
2020-04-30 22:35:05 +0400

    Added database to keep track of variables to avoid loss of data.

--- host_channel_creator/cogs/RaidHelper.py
+++ host_channel_creator/cogs/RaidHelper.py
@@ -5,32 +5,26 @@ import sqlite3
 
 
 class RaidHelper(commands.Cog, discord.Client):
-    # Class variables
-    channel_creator = None
-    new_chan = None
-    bot_user = None
-
-
 
     def __init__(self, client):
         self.client = client
-        self.bot_user = client.user
+        self.bot_user = client.user  # useless
 
     @commands.Cog.listener()
     async def on_ready(self):
         # Create the DB table if it does not exist on startup
         db = sqlite3.connect('RaidHelper.sqlite').cursor()
         db.execute(
-        '''
-        CREATE TABLE IF NOT EXISTS RaidHelper 
-        (
-            user_id	INTEGER NOT NULL,
-            user_name TEXT NOT NULL,
-            channel_id INTEGER NOT NULL,
-            channel_name INTEGER NOT NULL,
-            PRIMARY KEY(user_id)
-        )
-        ''')
+            '''
+            CREATE TABLE IF NOT EXISTS RaidHelper 
+            (
+                user_id	INTEGER NOT NULL,
+                user_name TEXT NOT NULL,
+                channel_id INTEGER NOT NULL,
+                channel_name INTEGER NOT NULL,
+                PRIMARY KEY(user_id)
+            )
+            ''')
         db.close()
         print('RaidHelper cog is loaded.')
 
@@ -79,13 +73,15 @@ class RaidHelper(commands.Cog, discord.Client):
                 new_chan = await ctx.guild.create_text_channel(chan_name, category=category)
                 # Set permissions for the user in this new channel
                 await new_chan.set_permissions(ctx.message.author, manage_messages=True)
-            # Setup and command list
-            commandembed = discord.Embed().set_author(name='List of Commands', icon_url='https://cdn.discordapp.com/attachments/704174855813070901/705216533923889263/491Darkrai.png')
-            commandembed.add_field(name='$mute', value='Used to mute a player', inline=False)
-            commandembed.add_field(name='$unmute', value='Used to unmute a player', inline=False)
-            commandembed.add_field(name='$ban', value='Used to ban a player from the channel', inline=False)
-            commandembed.add_field(name='$unban', value='Used to unban a player from the channel', inline=False)
-            await self.new_chan.send(embed=commandembed)
+                # Setup and command list
+                command_embed = discord.Embed().set_author(name='List of Commands',
+                                                           icon_url='https://cdn.discordapp.com/attachments/704174855813070901/705216533923889263/491Darkrai.png')
+                command_embed.add_field(name='$mute', value='Used to mute a player', inline=False)
+                command_embed.add_field(name='$unmute', value='Used to unmute a player', inline=False)
+                command_embed.add_field(name='$ban', value='Used to ban a player from the channel', inline=False)
+                command_embed.add_field(name='$unban', value='Used to unban a player from the channel', inline=False)
+                command_embed.add_field(name='$delete', value='Used to delete your channel', inline=False)
+                await new_chan.send(embed=command_embed)
                 # Write user and channel info into DB for later use
                 cursor.execute(
                     """
@@ -98,61 +94,75 @@ class RaidHelper(commands.Cog, discord.Client):
             cursor.close()
             db.close()
         else:
-            inputnameembed = discord.Embed(description='<:x_:705214517961031751> Please input a name for the channel after the command.')
-            await ctx.message.channel.send(embed=inputnameembed)
-
-    # Mute player
-    @commands.command()
-    @commands.has_role('Shiny Raid Host')
-    async def mute(self, ctx):
-        if ctx.message.mentions[0].permissions_in(self.new_chan).send_messages and self.channel_creator == ctx.message.author and ctx.channel == self.new_chan:
-            await self.new_chan.set_permissions(ctx.message.mentions[0], send_messages=False)
-            await ctx.message.delete()
-            disableembed = discord.Embed(description='<:SeekPng:705124992349896795> **Darkrai used Disable.** ***' + ctx.message.mentions[0].name + " no longer has permission to speak.***")
-            await self.new_chan.send(embed=disableembed)
-        elif self.channel_creator == ctx.message.author and ctx.channel == self.new_chan:
-            await ctx.message.delete()
-            mutedembed = discord.Embed(description='<:x_:705214517961031751> **Darkrai used Disable.** ***' + ctx.message.mentions[0].name + " already has no permission to speak.***")
-            await ctx.message.channel.send(embed=mutedembed)
-        # Need to add check for if member is already muted.
+            input_name_embed = discord.Embed(
+                description='<:x_:705214517961031751> Please input a name for the channel after the command.')
+            await ctx.message.channel.send(embed=input_name_embed)
 
 
-    # Unmute player
-    @commands.command()
-    @commands.has_role('Shiny Raid Host')
-    async def unmute(self, ctx):
-        if ctx.message.mentions[0].permissions_in(self.new_chan).send_messages and self.channel_creator == ctx.message.author and ctx.channel == self.new_chan:
-            await ctx.message.delete()
-            unmutedembed = discord.Embed(description='<:x_:705214517961031751> **Disable has already worn off for** ***' + ctx.message.mentions[0].name + "***")
-            await self.new_chan.send(embed=unmutedembed)
-        elif self.channel_creator == ctx.message.author and ctx.channel == self.new_chan:
-            await self.new_chan.set_permissions(ctx.message.mentions[0], send_messages=True)
-            await ctx.message.delete()
-            disableoffembed = discord.Embed(description='<:SeekPng:705124992349896795> **Disable has worn off for** ***' + ctx.message.mentions[0].name + " and they are now able to speak.***")
-            await self.new_chan.send(embed=disableoffembed)
+# Mute player
+@commands.command()
+@commands.has_role('Shiny Raid Host')
+async def mute(self, ctx):
+    if ctx.message.mentions[0].permissions_in(
+            self.new_chan).send_messages and self.channel_creator == ctx.message.author and ctx.channel == self.new_chan:
+        await self.new_chan.set_permissions(ctx.message.mentions[0], send_messages=False)
+        await ctx.message.delete()
+        disable_embed = discord.Embed(
+            description='<:SeekPng:705124992349896795> **Darkrai used Disable.** ***' + ctx.message.mentions[
+                0].name + " no longer has permission to speak.***")
+        await self.new_chan.send(embed=disable_embed)
+    elif self.channel_creator == ctx.message.author and ctx.channel == self.new_chan:
+        await ctx.message.delete()
+        muted_embed = discord.Embed(
+            description='<:x_:705214517961031751> **Darkrai used Disable.** ***' + ctx.message.mentions[
+                0].name + " already has no permission to speak.***")
+        await ctx.message.channel.send(embed=muted_embed)
+    # Need to add check for if member is already muted.
 
 
-    # Ban player from channel
-    @commands.command()
-    @commands.has_role('Shiny Raid Host')
-    async def ban(self, ctx):
-        if self.channel_creator == ctx.message.author:
-            await ctx.message.delete()
-            banembed = discord.Embed(description='<:blackhole:705225042052644915> **Darkrai used Dark Void.** ***' + ctx.message.mentions[0].mention + " has been banished to the void.***")
-            await self.new_chan.send(
-                'Darkrai used Dark Void. ' + ctx.message.mentions[0].mention + " has been banished to the void.")
-            await self.new_chan.set_permissions(ctx.message.mentions[0], read_messages=False)
+# Unmute player
+@commands.command()
+@commands.has_role('Shiny Raid Host')
+async def unmute(self, ctx):
+    if ctx.message.mentions[0].permissions_in(
+            self.new_chan).send_messages and self.channel_creator == ctx.message.author and ctx.channel == self.new_chan:
+        await ctx.message.delete()
+        unmuted_embed = discord.Embed(
+            description='<:x_:705214517961031751> **Disable has already worn off for** ***' + ctx.message.mentions[
+                0].name + "***")
+        await self.new_chan.send(embed=unmuted_embed)
+    elif self.channel_creator == ctx.message.author and ctx.channel == self.new_chan:
+        await self.new_chan.set_permissions(ctx.message.mentions[0], send_messages=True)
+        await ctx.message.delete()
+        disable_off_embed = discord.Embed(
+            description='<:SeekPng:705124992349896795> **Disable has worn off for** ***' + ctx.message.mentions[
+                0].name + " and they are now able to speak.***")
+        await self.new_chan.send(embed=disable_off_embed)
 
 
-    # Unban player from channel
-    @commands.command()
-    @commands.has_role('Shiny Raid Host')
-    async def unban(self, ctx):
-        if self.channel_creator == ctx.message.author:
-            await ctx.message.delete()
-            await self.new_chan.set_permissions(ctx.message.mentions[0], read_messages=True)
-            await self.new_chan.send(
-                ctx.message.mentions[0].mention + " has been pardoned and released from the void.")
+# Ban player from channel
+@commands.command()
+@commands.has_role('Shiny Raid Host')
+async def ban(self, ctx):
+    if self.channel_creator == ctx.message.author:
+        await ctx.message.delete()
+        ban_embed = discord.Embed(
+            description='<:blackhole:705225042052644915> **Darkrai used Dark Void.** ***' + ctx.message.mentions[
+                0].mention + " has been banished to the void.***")
+        await self.new_chan.send(
+            'Darkrai used Dark Void. ' + ctx.message.mentions[0].mention + " has been banished to the void.")
+        await self.new_chan.set_permissions(ctx.message.mentions[0], read_messages=False)
+
+
+# Unban player from channel
+@commands.command()
+@commands.has_role('Shiny Raid Host')
+async def unban(self, ctx):
+    if self.channel_creator == ctx.message.author:
+        await ctx.message.delete()
+        await self.new_chan.set_permissions(ctx.message.mentions[0], read_messages=True)
+        await self.new_chan.send(
+            ctx.message.mentions[0].mention + " has been pardoned and released from the void.")
 
 
 def setup(client):

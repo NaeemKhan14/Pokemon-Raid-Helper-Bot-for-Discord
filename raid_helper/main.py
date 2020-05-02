f5568e780eaca1b39ebcf435c90eef09c34663eb
Naeem Khan <naeemukhan14@gmail.com>
2020-05-02 04:50:45 +0400

    Removed unused functions

--- raid_helper/main.py
+++ raid_helper/main.py
@@ -1,23 +1,8 @@
 import os
 from discord.ext import commands
 
-TOKEN = os.getenv('DISCORD_TOKEN')  # Discord Token
-
+TOKEN = os.getenv('DISCORD_TOKEN')
 client = commands.Bot(command_prefix='$', case_insensitive=True)
-
-
-@client.command()
-@commands.has_role('admin')
-async def load(ctx, extension):
-    client.load_extension(f'cogs.{extension}')
-    print(f'{extension} has been loaded')
-
-
-@client.command()
-@commands.has_role('admin')
-async def unload(ctx, extension):
-    client.unload_extension(f'cogs.{extension}')
-    print(f'{extension} has been unloaded')
 
 
 @client.command()
@@ -26,6 +11,7 @@ async def reload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
     client.load_extension(f'cogs.{extension}')
     print(f'{extension} has been reloaded')
+    await ctx.message.delete()
 
 # Initially load all the cogs on startup
 for file in os.listdir('./cogs'):

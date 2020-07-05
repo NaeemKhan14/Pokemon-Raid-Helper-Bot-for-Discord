7e2f7665bbf4d057a8f674b581adf81f0f35cb71
Cooly4477 <cooly4477@gmail.com>
2020-07-05 18:42:13 -0500

    added random video command and revamped help menu

---
+++ raid_helper/cogs/randomvid.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+import json
+import urllib.request
+import string
+import random
+
+
+class RandomVid(commands.Cog, discord.Client):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('RandomVid cog is loaded.')
+
+    @commands.command()
+    async def video(self, ctx):
+        count = 1
+        API_KEY = 'AIzaSyA_hXrUpjM3cQ0Kc-oKPWMJf90iit_R0jc'
+        randoms = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(3))
+
+        urlData = "https://www.googleapis.com/youtube/v3/search?key={}&maxResults={}&part=snippet&type=video&q={}".format(
+            API_KEY, count, randoms)
+        webURL = urllib.request.urlopen(urlData)
+        data = webURL.read()
+        encoding = webURL.info().get_content_charset('utf-8')
+        results = json.loads(data.decode(encoding))
+
+        for data in results['items']:
+            videoId = (data['id']['videoId'])
+
+        await ctx.message.channel.send(embed=discord.Embed(description='https://www.youtube.com/watch?v=' + videoId))
+
+
+
+def setup(client):
+    client.add_cog(RandomVid(client))

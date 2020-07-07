ed798685f969ede7b63af71a0a15cbba0fe0d3f8
Cooly4477 <cooly4477@gmail.com>
2020-07-06 20:31:30 -0500

    added meme command and some organization

--- raid_helper/cogs/randomvid.py
+++ raid_helper/cogs/randomvid.py
@@ -18,12 +18,14 @@ class RandomVid(commands.Cog, discord.Client):
     @commands.command()
     async def video(self, ctx):
         count = 1
-        API_KEY = 'AIzaSyA_hXrUpjM3cQ0Kc-oKPWMJf90iit_R0jc'
+        API_KEY = 'AIzaSyAzHV9cVQXBzE6ydQ4kpYRZ6f55ZrMCfrA'
         randoms = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(3))
 
         urlData = "https://www.googleapis.com/youtube/v3/search?key={}&maxResults={}&part=snippet&type=video&q={}".format(
             API_KEY, count, randoms)
+
         webURL = urllib.request.urlopen(urlData)
+
         data = webURL.read()
         encoding = webURL.info().get_content_charset('utf-8')
         results = json.loads(data.decode(encoding))
